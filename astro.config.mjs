6041f65c3a5584e5d23e9e7d427091c84693956e
WebDevCodeBlue <webdevcodeblue@gmail.com>
2025-11-07 10:37:56 +0100

    promp-1-2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,18 @@ export default defineConfig({
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
-  }
+  },
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      i18n: {
+        defaultLocale: 'hr',
+        locales: {
+          hr: 'hr-HR',
+        },
+      },
+    })
+  ]
 });
